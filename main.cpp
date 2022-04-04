ba21721952e54996fdd95b61a22e48cbe76b217e
S1bery <45617892+S1bery@users.noreply.github.com>
2022-04-04 23:02:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,19 +9,24 @@ void full_bubbles (bool a[][12]) {
     }
 } // функция первичного заполнения
 
-bool check_coordinates(bool a[][12], int y, int x) {
+bool check_coordinates(bool a[][12], int y1, int x1, int y2, int x2) {
 
-    if (y < 0 || y > 11 ||
-        x < 0 || x > 11) {
+    if (y1 < 0 || y1 > 11 ||
+        x1 < 0 || x1 > 11 ||
+        y2 < 0 || y2 > 11 ||
+        x2 < 0 || x2 > 11) {
         return false;
         }
-    if (!a[y][x]) {
-        return false;
-        } else {
-        a[y][x] = false;
-        return true;
+
+    for (int i = y1; i <= y2; i++) {
+        for (int j = x1; j < x2; j++) {
+            if (!a[i][j]) {
+                return false;
+            }
         }
-} // проверяет корректность ввода координат и лопает пузырик
+    }
+    return true;
+} // проверяет корректность ввода координат
 
 void game_board(bool a[][12]) {
     for (int i = 0; i < 12; i++) {
@@ -42,25 +47,27 @@ int main() {
 
     game_board(bubble); // первыичный вывод поля
 
-    int y = 0; // столбец
-    int x = 0; // строка
+    int y1, x1, y2, x2 = 0;
     int endgame = 144; // общее количество пузырьков
 
     while(endgame != 0) {
-        std::cout << "Enter the coordinates:" << std::endl;
-        std::cin >> y >> x;
-        while (!check_coordinates(bubble, y, x)) {
+        std::cout << "Enter the first and second coordinates:" << std::endl;
+        std::cout << "Y X Y X" << std::endl;
+        std::cin >> y1 >> x1 >> y2 >> x2;
+        while (!check_coordinates(bubble, y1, x1, y2, x2)) {
             std::cout << "Error! Enter the coordinates:" << std::endl;
-            std::cin >> y >> x;
+            std::cout << "Y X Y X" << std::endl;
+            std::cin >> y1 >> x1 >> y2 >> x2;
         }
 
-        if (endgame == 0) {
-            break;
-        } else {
-            std::cout << "Pop!" << std::endl;
-            endgame -= 1;
-            game_board(bubble);
+        for (int i = y1; i <= y2; i++) {
+            for (int j = x1; j <= x2; j++) {
+                bubble[i][j] = false;
+                endgame -= 1;
+            }
         }
+        std::cout << "Pop!" << std::endl;
+        game_board(bubble);
     }
 
     return 0;
